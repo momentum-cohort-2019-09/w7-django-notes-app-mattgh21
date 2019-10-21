bbe9670969fdc9eeec428612e2d8e8d50279e4c7
Matt Gouchoe-Hanas <mattgh21@gmail.com>
2019-10-21 17:03:33 -0400

    adds urls and second view, runs server, displays notes

--- notes/views.py
+++ notes/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
-from data import NOTES
+from notes.data import NOTES
 
 # Create your views here.
-def notes_view(request):
+def notes_list(request):
     return render(request, 'notes/notes_list.html', {
         'notes': NOTES,
     })
 
 def notes_detail(request, id):
-    notes_list = NOTES[id]
+    note = NOTES[id]
     return render(request, 'notes/notes_detail.html', {
-        'notes_list': notes_list,
+        'note': note,
     })
