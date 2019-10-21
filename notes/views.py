be0b0fb944c806214adfb6a72753c6d932aeb3ef
Matt Gouchoe-Hanas <mattgh21@gmail.com>
2019-10-21 15:14:07 -0400

    adds templates and data file w/ notes dictionary

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from data import NOTES
 
 # Create your views here.
+def notes_view(request):
+    return render(request, 'notes/notes_list.html', {
+        'notes': NOTES,
+    })
+
+def notes_detail(request, id):
+    notes_list = NOTES[id]
+    return render(request, 'notes/notes_detail.html', {
+        'notes_list': notes_list,
+    })
