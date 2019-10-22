7846212de81cf39e13102647c82f845b332446bb
Matt Gouchoe-Hanas <mattgh21@gmail.com>
2019-10-22 14:35:50 -0400

    creates model, admin interface, migrates views and templates to model

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Note(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=100)
+    body = models.TextField()        
+    
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
